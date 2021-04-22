b1c73835b06d3af4a45fc167d3499aaaf4cbe7eb
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-22 08:12:07 +0300

    minor changes

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -8,17 +8,20 @@ export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // 👇 define the Lambda
     const myFunction = new NodejsFunction(this, 'my-function', {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'main',
       entry: path.join(__dirname, `/../src/my-lambda/index.ts`),
     });
 
+    // 👇 create a policy statement
     const s3ListBucketsPolicy = new iam.PolicyStatement({
       actions: ['s3:ListAllMyBuckets'],
       resources: ['arn:aws:s3:::*'],
     });
 
+    // 👇 add the policy to the Function's role
     myFunction.role?.attachInlinePolicy(
       new iam.Policy(this, 'list-buckets-policy', {
         statements: [s3ListBucketsPolicy],
